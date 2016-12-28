0b6b5f4ac1d73818b0b1a1e081078ff48e283373
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2016-12-28 23:56:38 +0100

    Test mocking data class

---
+++ kotlin-demo/src/test/kotlin/pl/touk/kotlin/dataclass.kt
@@ -0,0 +1,19 @@
+package pl.touk.kotlin
+
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+data class Developer(val firstName: String)
+
+class DataClassTest {
+
+    @Test
+    fun `should be mocked`() {
+        val dev = mock<Developer>()
+        whenever(dev.firstName).thenReturn("name")
+
+        assertThat(dev.firstName).isEqualTo("name")
+    }
+}
