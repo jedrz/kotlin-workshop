08f08a0cd4583c374f623c5268c3e16f538bfe16
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2017-01-16 15:40:10 +0100

    Add box examples

---
+++ kotlin-demo/src/main/kotlin/pl/touk/kotlin/Box.kt
@@ -0,0 +1,4 @@
+package pl.touk.kotlin
+
+
+class Box(private val value: Int)
---
+++ kotlin-demo/src/main/kotlin/pl/touk/kotlin/JavaBox.java
@@ -0,0 +1,13 @@
+package pl.touk.kotlin;
+
+public class JavaBox {
+    private final Integer value;
+
+    public JavaBox(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
